0e9cd98272db77e3d2fa705ba272f79ddd27b798
snowbot3 <snowbot3@gmail.com>
2017-08-15 08:22:21 -0500

    slowly working up to building. forces on the data layer currently.

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,10 +1,28 @@
-import * as db from 'nedb'
+import * as Nedb from 'nedb'
 
-class LinkData {
-	constructor(){
-		// const
+interface WallLink {
+	text: string
+	href: string
+}
+interface DataStoreOptions {
+	filename?: string
+	autoload?: boolean
+	timestampData?: boolean
+}
+Nedb.DataStoeOptions = DataStoreOptions;
+
+class WallLinks {
+	private db: Nedb
+	constructor(filename?:string){
+		this.db = new Nedb({
+			filename: filename,
+			autoload: true,
+			timestampData: true
+		})
 	}
-	getLinks(filter:string,start:number){
-		// get list stuff.
+	getLinks(filter:string,start:number) : WallLink[] {
+		return this.db.find()
 	}
 }
+
+export default new WallLink()
