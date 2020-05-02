8a70f79ecc72a26150d95cc4efda65d28587bbc5
snowbot3 <snowbot3@gmail.com>
2020-05-02 14:43:40 -0500

    broken test because of duplicate variable names

--- wall/js/dom.mjs
+++ wall/js/dom.mjs
@@ -1,6 +1,6 @@
 /** wall-dom **/
 
-import * as args from './args.mjs';
+import * as wall_args from './args.mjs';
 import { elem } from './elem.mjs';
 
 function convertCamelToDash(camel) {
@@ -9,12 +9,10 @@ function convertCamelToDash(camel) {
 
 // element templating
 export function dom(func) {
-    const names = args.names(func);
+    const names = wall_args.names(func);
     const args = names.map(function(name) {
         name = convertCamelToDash(name);
         return elem.bind(this, name);
     }, this);
     return func.apply(this, args);
 }
-
-//export { WallElem, elem };
