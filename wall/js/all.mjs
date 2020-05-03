7d5813bb2ecb8660b88b52fcf7fbae934b0e00ab
snowbot3 <snowbot3@gmail.com>
2020-05-03 11:14:46 -0500

    fix all.mjs: wall_func changed ot wall_args

--- wall/js/all.mjs
+++ wall/js/all.mjs
@@ -1,6 +1,6 @@
 
 export * as type from './type.mjs';
-export * as func from './func.mjs';
+export * as args from './args.mjs';
 export { elem } from './elem.mjs';
 export { dom } from './dom.mjs';
 
