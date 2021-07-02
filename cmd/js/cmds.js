cceb9e73dc1982c668d396474659c0146534896f
snowbot3 <snowbot3@gmail.com>
2021-07-01 20:41:42 -0500

    Remove unrelated files to separate the projects

--- cmd/js/cmds.js
+++
@@ -1,6 +0,0 @@
-// cmds
-export * from './cmds/simple.js';
-export * from './cmds/search.js';
-export * from './cmds/marc.js';
-
-// I could attempt import when a command is asked for, but then help would not work.
--- cmd/js/cmds/simple.js
+++
@@ -1,5 +0,0 @@
-// cmds
-// simple commands
-export function echo(...args) { return args.join(' '); }
-export function date() { return '' + new Date(); }
-export function clear() { location.reload(); return 'Please wait.'; }
